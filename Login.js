b01692dd0f54328eb8189aa62fab3a2d2fa1377c
Davidson-09 <davidsonakra@gmail.com>
2024-03-22 00:50:43 +0100

    initial commit

---
+++ Login.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const login = (props) =>{
+    return(
+        <Container>
+            <Nav>
+            </Nav>
+        </Container>
+    )
+}
+
+export default Login;
